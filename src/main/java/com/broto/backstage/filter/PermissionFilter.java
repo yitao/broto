c96d6ac269b067b58535c4a800915788c9bf809d
易滔 <528171154@qq.com>
2016-09-29 15:49:01 +0800

    permission-test

--- src/main/java/com/broto/backstage/filter/PermissionFilter.java
+++ src/main/java/com/broto/backstage/filter/PermissionFilter.java
@@ -1,7 +1,25 @@
 package com.broto.backstage.filter;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * Created by yitao on 2016/8/9.
  */
-public class PermissionFilter {
+public class PermissionFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
